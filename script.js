dc08d64693144190601fbb70fdd1c976842b1bb2
Leoddi <leonardo.deriva@gmail.com>
2022-06-04 17:08:43 +0200

    Migliorie mobile e effetti visivi

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const toggleButton = document.getElementsByClassName('toggleButton')[0];
 const navbarlinks = document.getElementsByClassName('navbarList')[0];
 const tema = document.getElementsByClassName('tema')[0];
 const feat = document.getElementsByClassName('feat')[0];
+const button = document.getElementsByClassName('center')[0];
+const titolo = document.getElementsByClassName('css-3d-text')[0];
+const sottotitolo = document.getElementsByClassName('css-3d-text-2')[0];
 
 
+button.addEventListener('click', ()=>{
+    titolo.classList.toggle('active');
+    sottotitolo.classList.toggle('active');
+})
 
 toggleButton.addEventListener('click', ()=>{
     navbarlinks.classList.toggle('active');
